4dcbc2e67e0dce294740d2a7f933ebe771fb4a50
marked01one <minhkhoitran2k3@gmail.com>
2024-12-04 20:13:54 -0800

    Day 3 Python

--- python/main.py
+++ python/main.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-
+import re
 
 def problem_01(puzzle_input: str):
     fp = open(puzzle_input, "r")
@@ -24,7 +24,7 @@ def problem_01(puzzle_input: str):
 
     result_B = sum([num * frequency[num] for n in set(left)])
 
-    return [result_A, result_B]
+    return result_A, result_B
 
 
 def problem_02(puzzle_input: str = "../02.txt"):
@@ -58,10 +58,61 @@ def problem_02(puzzle_input: str = "../02.txt"):
                 result_B += 1
                 break
     
-    return [result_A, result_B]
-    
-    
+    return result_A, result_B
     
 
-problem_02()
+def problem_03(puzzle_input: str = "../03.txt"):
+    REGEX = r"do\(\)|don't\(\)|mul\(\d+,\d+\)"
+    
+    fp = open(puzzle_input, 'r')
+    memory = fp.read()
+    
+    result_A, result_B = 0, 0
+    
+    numbers = '1234567890'
+    stages = ['', 'm', 'u', 'l', '(', numbers, ',', numbers, ')']
+    num1, num2 = '', ''
+    current = 0
+    
+    for i in range(len(memory)):
+        
+        # Attempt to move to next stage
+        if memory[i] in stages[current+1]:
+            current += 1
+        else:
+            # If `i` is not the next stage BUT belongs to one of the variable stages
+            # Remain at current stage
+            if (current == 5 or current == 7) and memory[i] in numbers: 
+                current = current
+            else:
+                num1, num2 = '', ''
+                # Move current stage back to 1 if its `m`
+                if memory[i] == 'm': current = 1
+                # Reset current stage otherwise    
+                else: current = 0
+            
+        match current:
+            case 8: result_A += int(num1) * int(num2); num1, num2 = '', ''; current = 0
+            case 7: num1 += memory[i]
+            case 5: num2 += memory[i]
+            case _: pass
+    
+    memory: list[str] = re.findall(REGEX, memory)
+    enabled = True
+    
+    for instruction in memory:
+        match instruction:
+            case "don't()": enabled = False
+            case "do()": enabled = True
+            case _: 
+                if enabled:
+                    num1, num2 = instruction[4:len(instruction)-1].split(',')
+                    result_B += int(num1) * int(num2)
+             
+    print(result_B)        
+    
+    return result_A, result_B  
+    
+
+problem_03()
         
