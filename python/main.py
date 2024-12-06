db0fd44cd557edb4257f4ff6e06047b2f2116a06
marked01one <minhkhoitran2k3@gmail.com>
2024-12-05 17:13:09 -0800

    Day 4 Python

--- python/main.py
+++ python/main.py
@@ -67,6 +67,8 @@ def problem_03(puzzle_input: str = "../03.txt"):
     fp = open(puzzle_input, 'r')
     memory = fp.read()
     
+    fp.close()
+    
     result_A, result_B = 0, 0
     
     numbers = '1234567890'
@@ -114,5 +116,97 @@ def problem_03(puzzle_input: str = "../03.txt"):
     return result_A, result_B  
     
 
-problem_03()
+def problem_04(puzzle_input: str = "../04.txt"):
+    fp = open(puzzle_input, 'r')
+    matrix = fp.readlines()
+    
+    result_A, result_B = 0, 0
+    
+    Xcoords, XMAScoords = [], []
+    
+    def is_xmas(line): return 1 if ''.join(line) == 'XMAS' else 0
+    
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 'X': Xcoords.append((i, j))
+    
+    for i in range(1, len(matrix)-1):
+        for j in range(1, len(matrix[i])-1):
+            if matrix[i][j] == 'A':
+                XMAScoords.append((i, j))
+    
+    for x, y in Xcoords:
+        N = is_xmas([matrix[x-i][y] for i in range(4)]) if x >= 3 else 0
+        S = is_xmas([matrix[x+i][y] for i in range(4)]) if (x <= len(matrix)-4) else 0 
+        W = is_xmas([matrix[x][y-j] for j in range(4)]) if (y >= 3) else 0
+        E = is_xmas([matrix[x][y+j] for j in range(4)]) if (y <= len(matrix)-4) else 0
+        
+        NW = is_xmas([matrix[x-i][y-i] for i in range(4)]) if (x >= 3 and y >= 3) else 0
+        NE = is_xmas([matrix[x-i][y+i] for i in range(4)]) if (x >= 3 and y <= len(matrix)-4) else 0
+        SW = is_xmas([matrix[x+i][y-i] for i in range(4)]) if (x <= len(matrix)-4 and y >= 3) else 0
+        SE = is_xmas([matrix[x+i][y+i] for i in range(4)]) if (x <= len(matrix)-4 and y <= len(matrix)-4) else 0
+        
+        result_A += sum([N, S, W, E, NW, NE, SW, SE])
+    
+    
+    for x, y in XMAScoords:
+        corners = matrix[x-1][y-1], matrix[x-1][y+1], matrix[x+1][y-1], matrix[x+1][y+1]
+        
+        if 'M' in corners and 'S' in corners and 'X' not in corners and 'A' not in corners:
+            if corners[0] == corners[1] and corners[0] != corners[2] and corners[1] != corners[3]:
+                result_B += 1
+            elif corners[0] == corners[2] and corners[0] != corners[1] and corners[0] != corners[3]:
+                result_B += 1 
+        
+    
+    return result_A, result_B
+    
+
+def problem_05(puzzle_input: str = "../05.txt"):
+    result_A, result_B = 0, 0
+    
+    fp = open(puzzle_input, 'r')
+    content = [line.replace('\n', '') for line in fp.readlines()]
+    fp.close()
+    
+    destinations = defaultdict(set)
+    origins = defaultdict(set)
+    unqualified_updates = []
+    
+    reached_arrays = False
+    
+    for line in content:
+        if line == '':
+            reached_arrays = True
+            continue
+        
+        if not reached_arrays:
+            src, dest = line.split('|')
+            destinations[int(src)].add(int(dest))
+            origins[int(dest)].add(int(src))
+        else:
+            updates = [int(i) for i in line.split(',')]
+            qualifies = True
+            for i in range(len(updates)):
+                before = origins[updates[i]].intersection(updates[:i]) if (i > 0) else set()
+                after = destinations[updates[i]].intersection(updates[i+1:]) if (i < len(updates)-1) else set()
+                
+                if len(before) != len(updates[:i]) or len(after) != len(updates[i+1:]): 
+                    qualifies = False
+                    unqualified_updates.append(updates)
+                    break
+            
+            result_A += updates[len(updates) // 2] if qualifies else 0
+    
+    for update in unqualified_updates:
+        pass
+    
+     
+    
+    
+    
+    
+    return result_A, result_B 
+
+problem_05() 
         
